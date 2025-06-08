9db78791fa4fa61b5f63123c199b02112ee82daf
rans0000 <rans0000@gmail.com>
2025-06-08 19:46:17 +0530

    change favicon

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,15 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import path from "path";
+import { ViteFaviconsPlugin } from "vite-plugin-favicon";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    ViteFaviconsPlugin("./public/images/out-thynk.png"),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
